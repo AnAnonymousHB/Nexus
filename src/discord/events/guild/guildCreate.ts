7668242b4bb0a6ef04bc13c3a2b058af7229570f
alima566 <aaronmai@hotmail.com>
2026-04-03 17:43:28 -0600

    Added guildCreate event

---
+++ src/discord/events/guild/guildCreate.ts
@@ -0,0 +1,12 @@
+import { Client, Events, Guild } from "discord.js";
+
+import { DiscordGuildManager, Logger } from "../../../managers/index.js";
+
+export default (client: Client) => {
+	client.on(Events.GuildCreate, async (guild: Guild) => {
+		await DiscordGuildManager.getSettings(guild.id);
+
+		const guildOwner = await guild.fetchOwner();
+		Logger.info("DISCORD_GUILD_CREATE", `Joined server ${guild.name} (${guild.id}). Owner: ${guildOwner.displayName} (${guild.ownerId})`);
+	});
+};
